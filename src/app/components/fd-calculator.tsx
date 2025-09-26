932f09f185d259c7a2901e91d58211f16bcda02e
Dhiyani S <dhiyanisrinivasan123@gmail.com>
2025-09-26 09:59:03 +0000

    Change the amount symbol into ₹

--- src/app/components/fd-calculator.tsx
+++ src/app/components/fd-calculator.tsx
@@ -76,12 +76,10 @@ export default function FdCalculator() {
 
   const formatCurrency = (amount: number) => {
     const formattedAmount = new Intl.NumberFormat("en-IN", {
-      style: 'currency',
-      currency: 'INR',
       minimumFractionDigits: 0,
       maximumFractionDigits: 0,
     }).format(amount);
-    return formattedAmount;
+    return `₹${formattedAmount}`;
   };
 
   const chartData = result
